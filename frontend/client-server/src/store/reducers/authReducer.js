aff9f91a61a0adb325da908cdb1736d0f1d8b308
Krutika Ipar <102004891+Krutikaipar@users.noreply.github.com>
2023-10-04 18:24:38 +0530

    Create authReducer.js

---
+++ frontend/client-server/src/store/reducers/authReducer.js
@@ -0,0 +1,20 @@
+import INITIAL_AUTH_STATE from '../fixtures/auth';
+import { HANDLE_SIGN_IN, HANDLE_SIGN_OUT } from '../actions/types';
+
+const authReducer = (state = INITIAL_AUTH_STATE, action) => {
+  switch (action.type) {
+    case HANDLE_SIGN_IN:
+    case HANDLE_SIGN_OUT:
+      let userId, hasSignedInState;
+      if (action.type === HANDLE_SIGN_IN) {
+        [userId, hasSignedInState] = [action.payload.userId, true];
+      } else if (action.type === HANDLE_SIGN_OUT) {
+        [userId, hasSignedInState] = [null, false];
+      }
+      return { ...state, hasSignedInState, userId };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
