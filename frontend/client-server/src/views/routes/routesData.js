d3c46f38eeb3684c3600ac3b5384e72550b17399
Krutika Ipar <102004891+Krutikaipar@users.noreply.github.com>
2023-10-04 18:56:29 +0530

    Create routesData.js

---
+++ frontend/client-server/src/views/routes/routesData.js
@@ -0,0 +1,41 @@
+import uuid from 'uuid';
+
+import StreamList from '../pages/streams/list';
+import StreamCreate from '../pages/streams/create';
+import StreamEdit from '../pages/streams/edit';
+import StreamDelete from '../pages/streams/delete';
+import StreamShow from '../pages/streams/show';
+
+const routesData = [
+  {
+    path: '/',
+    page: StreamList,
+    ownProps: {},
+  },
+  {
+    path: '/streams/create',
+    page: StreamCreate,
+    ownProps: {},
+  },
+  {
+    path: '/streams/:id(\\d+)',
+    page: StreamShow,
+    ownProps: {},
+  },
+  {
+    path: '/streams/:id(\\d+)/edit/',
+    page: StreamEdit,
+    ownProps: {},
+  },
+  {
+    path: '/streams/:id(\\d+)/delete',
+    page: StreamDelete,
+    ownProps: {},
+  },
+];
+
+routesData.forEach(function iterateRoutesData(item) {
+  item.uuid = uuid.v4();
+});
+
+export default routesData;
